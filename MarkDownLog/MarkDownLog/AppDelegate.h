6b9b9c0e670122ffa933961130dba4347ad80296
bamq <you@example.com>
2017-12-08 17:34:15 +0800

    增加示例工程

---
+++ MarkDownLog/MarkDownLog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MarkDownLog
+//
+//  Created by bamq on 2017/12/8.
+//  Copyright © 2017年 Aresoft (Shanghai) Tech Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MarkDownLog/MarkDownLog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MarkDownLog
+//
+//  Created by bamq on 2017/12/8.
+//  Copyright © 2017年 Aresoft (Shanghai) Tech Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
